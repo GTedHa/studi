51cc6884c9be6b21ecbeea419f99ffae6abab04c
Gyutae Ted Ha <gtha@yujinrobot.com>
2017-12-02 18:47:45 +0900

    Add proto of routing note page

--- studi/views.py
+++ studi/views.py
@@ -7,3 +7,9 @@ from studi import app
 def index():
     app.logger.warning('sample message')
     return render_template('index.html')
+
+@app.route('/note/<int:note_id>')
+def show_note_page(note_id):
+    note_name = 'Temp note'
+    return render_template('note.html', note_name=note_name)
+
