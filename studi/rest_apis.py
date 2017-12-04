99bc666b0caf94ed5672563af34d8615ee9bec1e
Gyutae Ted Ha <gtha@yujinrobot.com>
2017-12-05 00:14:00 +0900

    Create '/notes/list' post api proto

--- studi/rest_apis.py
+++ studi/rest_apis.py
@@ -4,3 +4,22 @@ from flask_restful import Api, Resource
 from studi import app
 
 api = Api(app)
+
+class Notes(Resource):
+
+    def __init__(self):
+        pass
+
+    def post(self):
+        message = ''
+        status = 400
+        dummys = []
+        for i in range(0, 3):
+            dummy = {
+                'note_id': 100+i,
+                'note_name': 'Note {0}'.format(i+1)
+            }
+            dummys.append(dummy)
+        return { 'notes': dummys }, 200
+
+api.add_resource(Notes, '/notes/list')
