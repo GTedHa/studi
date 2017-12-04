4ccb413631f0350b2a1585cdb26d8e9fd18f0203
Gyutae Ted Ha <gtha@yujinrobot.com>
2017-12-05 00:32:22 +0900

    Create '/clauses/list' post api proto

--- studi/rest_apis.py
+++ studi/rest_apis.py
@@ -1,5 +1,5 @@
 from flask import request, jsonify, Response
-from flask_restful import Api, Resource
+from flask_restful import reqparse, Api, Resource
 
 from studi import app
 
@@ -21,3 +21,27 @@ class Notes(Resource):
         return { 'notes': dummys }, 200
 
 api.add_resource(Notes, '/notes/list')
+
+class Clauses(Resource):
+
+    def __init__(self):
+        pass
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('note_id', type=int, help='Note ID')
+        args = parser.parse_args()
+        note_id = args['note_id']
+        dummys = []
+        for i in range(0, 10):
+            dummy = {
+                'clause_id': 10+i,
+                'title': 'Clause {0}'.format(i+1),
+                'contents': 'Clause {0} means nothing...'.format(i+1),
+                'imp': 0,
+                'und': 1
+            }
+            dummys.append(dummy)
+        return { 'note_id': note_id, 'clauses': dummys }, 200
+
+api.add_resource(Clauses, '/clauses/list')
