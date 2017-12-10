463e4ca97fcb061835e97a959897b1d0ab333ecb
Gyutae Ted Ha <gtha@yujinrobot.com>
2017-12-10 13:55:25 +0900

    Bump patch version up: 1.0.2 -> 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='studi',
-    version='1.0.2',
+    version='1.0.3',
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
