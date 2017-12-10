a230050c91564d5eae787ff8cf27a2d25dcd44f2
Gyutae Ted Ha <gtha@yujinrobot.com>
2017-12-10 13:51:55 +0900

    Change execute_sql func to execute sql script entirely

--- studi/utils.py
+++ studi/utils.py
@@ -1,22 +1,21 @@
 import logging
-from sqlite3 import OperationalError
+import sqlite3
 
 from studi import app
 
-def execute_sql(file_name, cur):
-    fd = open(file_name, 'r')
-    sql_file = fd.read()
-    fd.close()
-
-    sql_commands = sql_file.split(';')
-    app.logger.debug(sql_commands)
-    exe_cnt = 0
-    for command in sql_commands:
-        try:
-            cur.execute(command)
-            app.logger.debug("Success to call command: {0}".format(command))
-            exe_cnt += 1
-        except OperationalError as ope:
-            app.logger.debug("Fail to call command: {0}".format(command))
-            break
-    return (exe_cnt, len(sql_commands))
+def execute_sql_file(file_name, cur):
+    try:
+        fd = open(file_name, 'r')
+        sql_file = fd.read()
+        fd.close()
+    except Exception:
+        # Read file error.
+        app.logger.warning("Cannot open(read) .sql file.")
+        return False
+    try:
+        cur.executescript(sql_file)
+    except sqlite3.Error:
+        # Execute error.
+        app.logger.warning("Fail to execute {0}".format(file_name))
+        return False
+    return True
