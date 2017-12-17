3d940a1a9329acb51e5b37f761ee72f177250fc2
Gyutae Ted Ha <gtha@yujinrobot.com>
2017-12-18 00:30:48 +0900

    Add note_id to ClausePoints

--- studi/db/ClausePoints.sql
+++ studi/db/ClausePoints.sql
@@ -1,6 +1,8 @@
 CREATE TABLE ClausePoints (
     clause_id INTEGER PRIMARY KEY,
+    note_id INTEGER NOT NULL,
     imp INTEGER NOT NULL DEFAULT 0,
     und INTEGER NOT NULL DEFAULT 0,
-    FOREIGN KEY(clause_id) REFERENCES Clauses(clause_id)
+    FOREIGN KEY(clause_id) REFERENCES Clauses(clause_id),
+    FOREIGN KEY(note_id) REFERENCES Notes(note_id)
 );
