93428df3d4ceb2be334aeb7765cc7425fa88fdc6
Gyutae Ted Ha <gtha@yujinrobot.com>
2017-12-30 22:29:14 +0900

    Update views.py as updated dev docu
    - Remove clause page routing
    - Only note page routing

--- studi/views.py
+++ studi/views.py
@@ -5,15 +5,30 @@ from studi import app
 
 @app.route('/')
 def index():
-    app.logger.warning('sample message')
     return render_template('index.html')
 
 @app.route('/note/<int:note_id>')
 def show_note_page(note_id):
-    note_name = 'Temp note'
+    try:
+        rv = intf_db.query_db(
+            "SELECT note_name FROM Notes WHERE note_id={0}".format(note_id)
+        )
+    except Exception as exc:
+        app.logger.warn("Exception raised during 'SELECT note_name FROM Notes WHERE note_id={0}' query: {1}".format(
+            note_id, str(exc)
+        ))
+        return render_template('note.html', note_name='Wrong Note', note_id=note_id)
+    if rv:
+        try:
+            item = rv[0]
+            note_name = item['note_name']
+        except Exception as exc:
+            app.logger.warn(
+                "Exception raised during access to query row item {0}: {1}".format(
+                    item, str(exc)
+                )
+            )
+            note_name = 'Wrong note'
+    else:
+        note_name = 'Note not found'            
     return render_template('note.html', note_name=note_name, note_id=note_id)
-
-@app.route('/note/<int:note_id>/clause')
-def show_clause_page(note_id):
-    note_id = 1
-    return render_template('clause.html', note_id=note_id)
