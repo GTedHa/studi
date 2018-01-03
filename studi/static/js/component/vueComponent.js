6495f1460784d0a9928c62a15275d44f3b11f50d
histuckyi <histuckyi@gmail.com>
2018-01-03 22:06:46 +0900

    Apply vue.js to note.html

--- studi/static/js/component/vueComponent.js
+++ studi/static/js/component/vueComponent.js
@@ -1,13 +1,23 @@
-new Vue({
-    el : "#notes",
-    data : {
+console.log('vueComponent.js')
+// 등록
+Vue.component('my-component', {
+    template: '<div>사용자 정의 컴포넌트 입니다!</div>'
+  })
+  
+  // 루트 인스턴스 생성
+  new Vue({
+    el: '#example'
+  })
 
-    },
-    component : {
-
-    }
-})
-Vue.component('my-component',{
-    template : '<div>테스트</div>'
-})
+  new Vue({
+      el : '#notes',
+      data : {
+          timer : 0, // timer
+          noteId : null,
+          istart : 0, // 0 : 
+      },
+      created : function(){
+          console.log('created!!');
+      }
+  })
 
