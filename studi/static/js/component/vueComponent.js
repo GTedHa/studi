671552f9b0c58852bcdbf114d39e2e27c005bac0
histuckyi <histuckyi@gmail.com>
2018-01-04 00:36:20 +0900

    Modify update vue and css

--- studi/static/js/component/vueComponent.js
+++ studi/static/js/component/vueComponent.js
@@ -13,11 +13,30 @@ Vue.component('my-component', {
       el : '#notes',
       data : {
           timer : 0, // timer
+          title : 'title',
           noteId : null,
-          istart : 0, // 0 : 
+          imp : [], // 0 : 안중요, 1 : 중요
+          und : [], // 0 : 이해x, 1 : 보통,  2 : 이해 o
+          clauseList : [],
+          selectedClauseList : []
       },
       created : function(){
           console.log('created!!');
+          var noteId = localStorage.getItem('noteId');
+          var noteName = localStorage.getItem('noteName');
+          // 노트에 대한 정보 호출
+        //   $.ajax({
+        //       url : "note/",
+        //   })
+      },
+      methods : {
+          start : function(){
+              console.log('start');
+          },
+          next : function(){
+              // 조건 조회하여 
+              // 다음 퀴즈 보여주기
+          }
       }
   })
 
