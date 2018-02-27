d96ae42322bc7e649605b0d96beb19dcf5b5189e
histuckyi <histuckyi@gmail.com>
2018-02-27 21:25:04 +0900

    Debug remove '>' character in localStorage noteName

--- studi/static/js/index.js
+++ studi/static/js/index.js
@@ -16,11 +16,11 @@ $(document).ready(function(){
          contentType: false, // NEEDED, DON'T OMIT THIS (requires jQuery 1.6+)
          processData: false, // NEEDED, DON'T OMIT THIS
          success : function(data, textStatus, xhr){
-           aler('업로드 성공');
+           alert('업로드 성공');
            location.reload(); // 화면 재시작하여 note list 다시 가져오기
          },
          error : function(error){
-          alert('');
+          alert('업로드 실패');
          }
        })
      })
@@ -28,8 +28,8 @@ $(document).ready(function(){
      $(document).on('click', '.list-group-item' ,function(){
     
       var noteId = $(this).data('id');
-      var noteName = $(this).text();
- 
+      var noteName = $(this).children('.noteName-item').text();
+
       // list click 시에 localStrorage에 저장
      localStorage.setItem('noteId', noteId);
      localStorage.setItem('noteName', noteName);
