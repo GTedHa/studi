e342ca5bc5451e148cda5a162652bb95af72b842
Daim Tak <histuckyi@gmail.com>
2019-05-19 17:37:09 +0900

    Modify tests/* exception handling

--- tests/test_create_db.py
+++ tests/test_create_db.py
@@ -1,14 +1,15 @@
 import logging
 import os
+import sqlalchemy
 import unittest
 
-import studi
-from studi import sqlalchemy
+from studi import app
+from studi import sqlalchemy_orm, util, custom_error
 
 def gen_logger(test_name):
     logger = logging.getLogger(test_name)
     logger.setLevel(logging.DEBUG)
-    logger_handler = logging.FileHandler(os.path.join(studi.app.config['LOG_DIR'], test_name + '.log'))
+    logger_handler = logging.FileHandler(os.path.join(app.config['TEST_LOG_DIR'], test_name + '.log'))
     logger_handler.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger_handler.setFormatter(formatter)
@@ -18,46 +19,75 @@ def gen_logger(test_name):
 test_name = "test_create_db"
 logger = gen_logger(test_name)
 
-
 # Testing that create DB by using flask_sqlalchemy(ORM)
 class TestCreateDB(unittest.TestCase):
 
     def setUp(self):
-        studi.app.testing = True
-        studi.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/test_studi.db'
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/test_studi.db'
+
 
         # delete db
         if os.path.exists("../studi/db/test_studi.db"):
-            sqlalchemy.drop_db(False)
-            self.app = studi.app.test_client()
+            sqlalchemy_orm.drop_db(False)
+            self.app = app.test_client()
 
 
     def test_create_db(self):
-        with studi.app.app_context():
+        with app.app_context():
             try:
-                db = sqlalchemy.create_db(False)
-            except Exception as exc:
-                logger.debug("Cannot create db, Error : {0}".format(exc))
-            finally:
+                db = sqlalchemy_orm.create_db(False)
                 table_names = db.engine.table_names()
                 self.assertIn('notes', table_names)
                 self.assertIn('clauses', table_names)
                 self.assertIn('clause_points', table_names)
 
+            except sqlalchemy.exc.SQLAlchemyError:
+                error_message = util.traceback_custom_error()
+                error = custom_error.SQLAlchemyError(error_message)
+                error.set_logger(logger)
+                error.to_dict()
+                raise
+            except AssertionError:
+                error_message = util.traceback_custom_error()
+                error = custom_error.AssertionError(error_message)
+                error.set_logger(logger)
+                error.to_dict()
+                raise
+            except:
+                error_message = util.traceback_custom_error()
+                error = custom_error.UnExpectedError(error_message)
+                error.set_logger(logger)
+                error.to_dict()
+                raise
+
+
     def test_delete_db(self):
-        with studi.app.app_context():
+        with app.app_context():
             try:
-                db = sqlalchemy.drop_db(False)
-            except Exception as exc:
-                logger.debug('Cannot delete db, Error : {1}'.format(exc))
-            finally:
+                db = sqlalchemy_orm.drop_db(False)
                 table_names = db.engine.table_names()
                 self.assertEqual(table_names, [])
                 self.assertEqual(len(table_names), 0)
 
+            except sqlalchemy.exc.SQLAlchemyError:
+                error_message = util.traceback_custom_error()
+                error = custom_error.SQLAlchemyError(error_message)
+                error.set_logger(logger)
+                error.to_dict()
+                raise
+            except AssertionError:
+                error_message = util.traceback_custom_error()
+                error = custom_error.AssertionError(error_message)
+                error.set_logger(logger)
+                error.to_dict()
+                raise
+            except:
+                error_message = util.traceback_custom_error()
+                error = custom_error.UnExpectedError(error_message)
+                error.set_logger(logger)
+                error.to_dict()
+                raise
 
-    def tearDown(self):
-        studi.app.testing = False
 
 if __name__ == "__main__":
     unittest.main()
--- tests/test_delete.py
+++ tests/test_delete.py
@@ -1,16 +1,17 @@
 import logging
 import os
+import sqlalchemy
 import unittest
 
 from studi import app
-from studi import sqlalchemy
-from studi import upload
-from studi.sqlalchemy import Notes, Clauses, ClausePoints
+from studi import sqlalchemy_orm, upload, util, custom_error
+from studi.sqlalchemy_orm import Notes, Clauses, ClausePoints
+
 
 def gen_logger(test_name):
     logger = logging.getLogger(test_name)
     logger.setLevel(logging.DEBUG)
-    logger_handler = logging.FileHandler(os.path.join(app.config['LOG_DIR'], test_name + '.log'))
+    logger_handler = logging.FileHandler(os.path.join(app.config['TEST_LOG_DIR'], test_name + '.log'))
     logger_handler.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger_handler.setFormatter(formatter)
@@ -27,13 +28,12 @@ class TestDeleteDB(unittest.TestCase):
     """
     def setUp(self):
         self.app = app.test_client()
-        app.testing = True
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/test_studi.db'
 
         #create new tesint DB & insert dummy data
         if os.path.exists("../studi/db/test_studi.db"):
-            sqlalchemy.drop_db(False)
-        sqlalchemy.create_db(False)
+            sqlalchemy_orm.drop_db(False)
+        sqlalchemy_orm.create_db(False)
         upload.insert_csv_to_db(False)  # note_id : 1
         upload.insert_csv_to_db(False)  # note_id : 2
         upload.insert_csv_to_db(False)  # note_id : 3
@@ -44,18 +44,33 @@ class TestDeleteDB(unittest.TestCase):
     def test_delete_one_note(self):
         with app.app_context():
             try:
-                sqlalchemy.delete_note_and_related_data_from_db(3)
-            except Exception as exc:
-                logger.debug('Cannot delete note and related data from other tables, because {0}'.format(exc))
-            finally:
+                sqlalchemy_orm.delete_note_and_related_data_from_db(3)
                 # Check if result is empty
-                note_result = sqlalchemy.get_item_from_db(Notes, {'note_id' : 3})
-                clauses_result = sqlalchemy.get_item_from_db(Clauses, {'note_id' : 3})
-                clausePoints_result = sqlalchemy.get_item_from_db(ClausePoints, {'note_id' : 3})
+                note_result = sqlalchemy_orm.get_item_from_db(Notes, {'note_id' : 3})
+                clauses_result = sqlalchemy_orm.get_item_from_db(Clauses, {'note_id' : 3})
+                clausePoints_result = sqlalchemy_orm.get_item_from_db(ClausePoints, {'note_id' : 3})
                 self.assertEqual(note_result, [])
                 self.assertEqual(clauses_result, [])
                 self.assertEqual(clausePoints_result, [])
 
+            except sqlalchemy.exc.SQLAlchemyError:
+                error_message = util.traceback_custom_error()
+                error = custom_error.SQLAlchemyError(error_message)
+                error.set_logger(logger)
+                error.to_dict()
+                raise
+            except AssertionError:
+                error_message = util.traceback_custom_error()
+                error = custom_error.AssertionError(error_message)
+                error.set_logger(logger)
+                error.to_dict()
+                raise
+            except:
+                error_message = util.traceback_custom_error()
+                error = custom_error.UnExpectedError(error_message)
+                error.set_logger(logger)
+                error.to_dict()
+                raise
 
 
     # Delete multiple Note (with related clause, clausePoints Data)
@@ -63,13 +78,29 @@ class TestDeleteDB(unittest.TestCase):
     def test_delete_multiple_note(self):
         with app.app_context():
             try:
-                sqlalchemy.delete_data_from_db(Notes, {'note_id' : [1, 2]})
-            except Exception as exc:
-                logger.debug("Cannot delete multiple notes data, because : {0}".format(exc))
-            finally:
+                sqlalchemy_orm.delete_data_from_db(Notes, {'note_id' : [1, 2]})
                 # Check if result is empty
-                note_result = sqlalchemy.get_item_from_db(Notes, {'note_id' : [1, 2]})
+                note_result = sqlalchemy_orm.get_item_from_db(Notes, {'note_id' : [1, 2]})
                 self.assertEqual(note_result, [])
+
+            except sqlalchemy.exc.SQLAlchemyError:
+                error_message = util.traceback_custom_error()
+                error = custom_error.SQLAlchemyError(error_message)
+                error.set_logger(logger)
+                error.to_dict()
+                raise
+            except AssertionError:
+                error_message = util.traceback_custom_error()
+                error = custom_error.AssertionError(error_message)
+                error.set_logger(logger)
+                error.to_dict()
+                raise
+            except:
+                error_message = util.traceback_custom_error()
+                error = custom_error.UnExpectedError(error_message)
+                error.set_logger(logger)
+                error.to_dict()
+                raise
 
 
     # Delete Clause (with related clausePoints)
@@ -77,20 +108,33 @@ class TestDeleteDB(unittest.TestCase):
     def test_delete_clause(self):
         with app.app_context():
             try:
-                sqlalchemy.delete_data_from_db(Clauses, {'clause_id' : 11})
-            except Exception as exc:
-                logger.debug("Cannot delete clause and related points data, baluse : {0}".format(exc))
-            finally:
-                clause_result = sqlalchemy.get_item_from_db(Clauses, {'clause_id' : 11})
-                clause_point_result = sqlalchemy.get_item_from_db(ClausePoints, {'clause_id' : 11})
+                sqlalchemy_orm.delete_data_from_db(Clauses, {'clause_id' : 11})
+                clause_result = sqlalchemy_orm.get_item_from_db(Clauses, {'clause_id' : 11})
+                clause_point_result = sqlalchemy_orm.get_item_from_db(ClausePoints, {'clause_id' : 11})
 
                 # Check if result is empty
                 self.assertEqual(clause_result,[])
                 self.assertEqual(clause_point_result, [])
 
+            except sqlalchemy.exc.SQLAlchemyError:
+                error_message = util.traceback_custom_error()
+                error = custom_error.SQLAlchemyError(error_message)
+                error.set_logger(logger)
+                error.to_dict()
+                raise
+            except AssertionError:
+                error_message = util.traceback_custom_error()
+                error = custom_error.AssertionError(error_message)
+                error.set_logger(logger)
+                error.to_dict()
+                raise
+            except:
+                error_message = util.traceback_custom_error()
+                error = custom_error.UnExpectedError(error_message)
+                error.set_logger(logger)
+                error.to_dict()
+                raise
 
-    def tearDown(self):
-        app.testing = False
 
 if __name__ == "__main__":
     unittest.main()
